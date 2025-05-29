fix(base): improved numerical stability of the solution by rounding floats to 14 decimal places

tf2ss/base.py
@@ -387,8 +387,12 @@ def tf2ss(
     if axis is not None:
         numerators_ = trim_zeros_along_axis(numerators_, axis=axis, trim="b")
     # Round to avoid floating point errors. Highest stable precision is 14 decimal places.
-    numerators_ = np.vectorize(lambda x: sp.Rational(str(x)))(numerators_)
-    denominators_ = np.vectorize(lambda x: sp.Rational(str(x)))(denominators_)
+    numerators_ = np.vectorize(lambda x: sp.Rational(str(round(x, 14))))(
+        numerators_
+    )
+    denominators_ = np.vectorize(lambda x: sp.Rational(str(round(x, 14))))(
+        denominators_
+    )
     n_outputs = len(numerators_)
     n_inputs = len(numerators_[0])
     s = sp.Symbol("s")